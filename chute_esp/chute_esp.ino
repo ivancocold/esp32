51da50c07c1b9f1d6ba7ce386ace93a0a62c4fd4
Ivan-Corneille MAGAGI <ivancocold@gmail.com>
2024-06-03 23:56:08 +0200

    Mise à jour des fonctionnalités.

--- chute_esp/chute_esp.ino
+++ chute_esp/chute_esp.ino
@@ -1,16 +1,6 @@
 // Import de la bibliothèque painlessMesh
 #include "painlessMesh.h"
 #include <ArduinoBLE.h>
-//************************************************************
-// Utilisation de la bibliothèque painlessMesh
-//
-// 1. Envoie un message précis à chaque nœud du réseau
-// 2. Affiche tout ce qu'il reçoit sur Serial.print
-// 
-//
-//
-//************************************************************
-
 
 // Nom du réseau
 #define MESH_PREFIX "CotonLePlusBeau"
@@ -21,11 +11,12 @@
 #define MESH_PORT 5555
 
 // Déclaration des variables globales
-uint32_t nodeId;
-bool lastButtonState = LOW;  // Pour suivre l'état précédent du bouton
-unsigned long lastHighTime = 0;  // Pour suivre le temps depuis que le bouton est HIGH
+uint32_t nodeId; //ID du noeud dans le réseau.
+bool lastButtonState = LOW;  // Pour suivre l'état précédent de la pin.
+unsigned long lastHighTime = 0;  // Pour suivre le temps depuis lequel la pin est HIGH
 int reinit = 5000; //Temps d'attente entre 2 messages d'un même noeud en millisecondes.
 unsigned long activationCount  = 0; //Nombre de chutes
+String NodeName="M. MAGAGI";
 
 // Objet Scheduler pour contrôler les tâches personnalisées
 Scheduler userScheduler;
@@ -36,10 +27,33 @@ painlessMesh mesh;
 // Prototype de la fonction pour envoyer des messages
 void sendMessage();
 
-// Définition de la tâche d'envoie de message en boucle infinie.
+// Définition de la tâche d'envoi de message en boucle infinie.
 Task taskSendMessage(TASK_SECOND * 1, TASK_FOREVER, &sendMessage);
 
-//Définition des fonctions
+// Déclaration des caractéristiques BLE
+BLEService resetService("180F"); // UUID du service
+BLEByteCharacteristic resetCharacteristic("2A19", BLEWrite| BLERead); // UUID de la caractéristique pour réinitialisation
+BLEUnsignedLongCharacteristic countCharacteristic("2A1C", BLERead); // UUID de la caractéristique de compteur
+BLEUnsignedLongCharacteristic nodeIdCharacteristic("2A1D", BLERead); // UUID de la caractéristique d'ID de nœud
+BLEUnsignedLongCharacteristic connectionCountCharacteristic("2A1E", BLERead); // UUID de la caractéristique de nombre de connexions
+BLEStringCharacteristic connectionStatusCharacteristic("2A1F", BLERead, 20); // UUID de la caractéristique de statut de connexion
+
+void setupCharacteristics()
+{
+  // Déclaration des descripteurs
+  BLEDescriptor resetDescriptor("2A19", "Reset Command");
+  BLEDescriptor countDescriptor("2A1C", "Compteur de chutes");
+  BLEDescriptor nodeIdDescriptor("2A1D", "Node ID");
+  BLEDescriptor connectionCountDescriptor("2A1E", "Nombre de connexions");
+  BLEDescriptor connectionStatusDescriptor("2A1F", "Etat de la connexion");
+
+  // Ajouter des descripteurs pour les caractéristiques BLE
+  resetCharacteristic.addDescriptor(resetDescriptor);
+  countCharacteristic.addDescriptor(countDescriptor);
+  nodeIdCharacteristic.addDescriptor(nodeIdDescriptor);
+  connectionCountCharacteristic.addDescriptor(connectionCountDescriptor);
+  connectionStatusCharacteristic.addDescriptor(connectionStatusDescriptor);
+}
 
 // Fonction pour envoyer des messages
 void sendMessage()
@@ -51,7 +65,7 @@ void sendMessage()
   {
     nodeId = mesh.getNodeId();
     // Message à envoyer
-    String msg = "Chute; Mr MAGAGI est tombé pour la "+ String(activationCount)+"ème fois de la journée. ID : " + String(nodeId);
+    String msg = "Chute; "+ NodeName +" est tombé pour la "+ String(activationCount)+"è fois de la journée. ID : " + String(nodeId);
     // Envoi du message à tous les nœuds du wifi-mesh
     mesh.sendBroadcast(msg);
     Serial.println(msg);  // Envoie le message via le port série
@@ -64,17 +78,17 @@ void receivedCallback(uint32_t from, String &msg)
 {
   // Affiche le message reçu et l'ID de l'expéditeur
   if (msg.length() > 0)
-    {
-      //Affiche le message reçu.
-      Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-      // Envoie le message via le port série
-      Serial.println(msg);
-      delay(20000);  // Attendre 20 secondes avant d'envoyer le prochain message
-    }
+  {
+    // Affiche le message reçu.
+    Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+    // Envoie le message via le port série
+    Serial.println(msg);
+    delay(20000);  // Attendre 20 secondes avant d'envoyer le prochain message
+  }
   else
-    {
-      Serial.printf("startHere: Received an empty message from %u\n", from);
-    }
+  {
+    Serial.printf("startHere: Received an empty message from %u\n", from);
+  }
 }
 
 // Callback pour les nouvelles connexions
@@ -82,6 +96,8 @@ void newConnectionCallback(uint32_t nodeId)
 {
   // Affiche l'ID du nouveau nœud connecté
   Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+  connectionCountCharacteristic.writeValue(mesh.getNodeList().size());
+  connectionStatusCharacteristic.writeValue("Connected");
 }
 
 // Callback pour les changements de connexion
@@ -89,6 +105,7 @@ void changedConnectionCallback()
 {
   // Affiche un message lorsque les connexions changent
   Serial.printf("Changed connections\n");
+  connectionCountCharacteristic.writeValue(mesh.getNodeList().size());
 }
 
 // Callback pour l'ajustement du temps des nœuds
@@ -97,13 +114,8 @@ void nodeTimeAdjustedCallback(int32_t offset) {
   Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
 }
 
-// Déclaration du service et de la caractéristique BLE
-BLEService resetService("180F"); // UUID du service
-BLEByteCharacteristic resetCharacteristic("2A19", BLERead | BLEWrite); // UUID de la caractéristique
-BLEUnsignedLongCharacteristic countCharacteristic("2A1C", BLERead); // UUID de la caractéristique de compteur
-
 // Fonction de configuration
-void setup() 
+void setup()
 {
   // Initialisation de la communication série à 115200 bauds
   Serial.begin(115200);
@@ -115,18 +127,30 @@ void setup()
   // Configuration BLE
   if (!BLE.begin())
   {
-  Serial.println("starting BLE failed!");
-  while (1);
+    Serial.println("starting BLE failed!");
+    while (1);
   }
 
-   // Configurez le périphérique BLE
-  BLE.setLocalName("MAGAGI");
+  // Configurez le périphérique BLE
+
+  // Conversion de NodeName en char array
+  char localName[NodeName.length() + 1];
+  NodeName.toCharArray(localName, NodeName.length() + 1);
+
+  BLE.setLocalName(localName);
   BLE.setAdvertisedService(resetService);
   resetService.addCharacteristic(resetCharacteristic);
   resetService.addCharacteristic(countCharacteristic);
+  resetService.addCharacteristic(nodeIdCharacteristic);
+  resetService.addCharacteristic(connectionCountCharacteristic);
+  resetService.addCharacteristic(connectionStatusCharacteristic);
+  setupCharacteristics(); // Appeler la fonction pour ajouter les descripteurs
   BLE.addService(resetService);
   resetCharacteristic.writeValue(0);
   countCharacteristic.writeValue(activationCount);
+  nodeIdCharacteristic.writeValue(mesh.getNodeId());
+  connectionCountCharacteristic.writeValue(mesh.getNodeList().size());
+  connectionStatusCharacteristic.writeValue("Disconnected");
   BLE.advertise();
   Serial.println("Bluetooth en attente de connexions...");
 
@@ -160,68 +184,70 @@ void setup()
 // Fonction de boucle principale
 void loop()
 {
-  // it will run the user scheduler as well
   // Mise à jour du réseau mesh
   mesh.update();
   // Mise à jour BLE
   BLE.poll();
-  
+
   // Vérifiez si la caractéristique de réinitialisation a été écrite
-if (resetCharacteristic.written())
-{
-  // Lire la valeur de la caractéristique en tant qu'octets
-  uint8_t buf[20]; // 20 octets maximum
-  int bytesRead = resetCharacteristic.readValue(buf, sizeof(buf));
-
-  if (bytesRead > 0)
+  if (resetCharacteristic.written())
   {
-    // Convertir les octets en une chaîne de caractères
-    String value = "";
-    for (int i = 0; i < bytesRead; i++)
-    {
-      value += (char)buf[i];
-    }
+    // Lire la valeur de la caractéristique en tant qu'octets
+    uint8_t buf[20]; // 20 octets maximum
+    int bytesRead = resetCharacteristic.readValue(buf, sizeof(buf));
 
-    // Vérifier si la valeur est "futa"
-    if (value == "f")
+    if (bytesRead > 0)
     {
-      activationCount=0;
-      Serial.println("Réinitialisation réussie. ");  // Envoyer le message "reset" sur le port série
-      // Réinitialiser la caractéristique à une chaîne vide après la réinitialisation
-      resetCharacteristic.writeValue((uint8_t)0);
-    }
-    else if (value == "0")
-    {
-      Serial.println("Reset BDD. "+ String(nodeId)); // Envoyer le message "reset" sur le port série
-    }
-    else if (value == "a")
-    {
-      Serial.println("Reset all."); // Envoyer le message "reset" sur le port série
+      // Convertir les octets en une chaîne de caractères
+      String value = "";
+      for (int i = 0; i < bytesRead; i++)
+      {
+        value += (char)buf[i];
+      }
+
+      // Vérifier si la valeur est "f"
+      if (value == "f")
+      {
+        activationCount = 0;
+        Serial.println("Réinitialisation réussie.");  // Envoyer le message "Réinitialisation réussie." sur le port série
+        // Réinitialiser la caractéristique à une chaîne vide après la réinitialisation
+        resetCharacteristic.writeValue((uint8_t)0);
+      }
+      else if (value == "0")
+      {
+        Serial.println("Reset BDD. " + String(nodeId)); // Envoyer le message "Reset BDD." sur le port série
+      }
+      else if (value == "a")
+      {
+        Serial.println("Reset all."); // Envoyer le message "Reset all." sur le port série
+      }
     }
   }
-}
 
   // Lire l'état actuel du bouton
   int currentButtonState = digitalRead(1);
 
   // Vérifier si le bouton vient d'être pressé (transition de LOW à HIGH)
   if (currentButtonState == HIGH && lastButtonState == LOW)
-    {
-      activationCount++; // Incrémenter le compteur
-      countCharacteristic.writeValue(activationCount); // Mettre à jour la caractéristique
-      // Envoyer un message lorsque le bouton est pressés
-      sendMessage();
-      // Enregistrer le moment où le bouton a été pressé
-      lastHighTime = millis();
-    }
+  {
+    activationCount++; // Incrémenter le compteur
+    countCharacteristic.writeValue(activationCount); // Mettre à jour la caractéristique
+    // Envoyer un message lorsque le bouton est pressé
+    sendMessage();
+    // Enregistrer le moment où le bouton a été pressé
+    lastHighTime = millis();
+  }
 
   // Si le bouton est maintenu enfoncé, vérifier le temps écoulé
   if (currentButtonState == HIGH && lastButtonState == HIGH)
+  {
+    if (millis() - lastHighTime >= reinit)  // 5 secondes
     {
-      if (millis() - lastHighTime >= reinit)  // 5 secondes
-        {
-          // Réinitialiser lastButtonState à LOW après 5 secondes
-          lastButtonState = LOW;
-        }
+      // Réinitialiser lastButtonState à LOW après 5 secondes
+      lastButtonState = LOW;
     }
+  }
+
+  // Mettre à jour lastButtonState
+  lastButtonState = currentButtonState;
 }
--- script.sql
+++ script.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `chute` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `chute`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: chute
@@ -28,7 +30,7 @@ CREATE TABLE `messages` (
   `message` text,
   `patient` text,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=180 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=191 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -74,4 +76,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-03 14:59:39
+-- Dump completed on 2024-06-03 16:10:44
